714ab09a425991778fef2e4f45d175eb74635631
nam270903 <nam270903@gmail.com>
2023-12-11 20:30:41 +0700

    add items-lis to notification

--- src/pages/Notification.tsx
+++ src/pages/Notification.tsx
@@ -6,7 +6,12 @@ import {
     IonContent,
     IonButton,
     IonButtons,
-    IonMenuButton
+    IonMenuButton,
+    IonList,
+    IonLabel,
+    IonItem, 
+    IonInfiniteScroll,
+    IonInfiniteScrollContent
 } from '@ionic/react'
 import '@capacitor/push-notifications'
 import React, {useEffect, useState} from 'react'
@@ -17,8 +22,23 @@ const Notification: React.FC = () => {
         sendTelegramNotification('This is where they attack us');
       };
     
+    const [items, setItems] = useState<string[]>([]);
+    
+    const generateItems = () => {
+        const newItems = [];
+        for (let i = 0; i < 51; i++) {
+            newItems.push(`Item ${1 + items.length + i}`);
+        }
+        setItems([...items, ...newItems]);
+    };
+
+    useEffect(() => {
+        generateItems();
+      }, []);
+
     return (
         <IonPage>
+
             <IonHeader>
                 <IonToolbar>
                     <IonButtons slot='start'>
@@ -27,12 +47,26 @@ const Notification: React.FC = () => {
                     <IonTitle>Notifications</IonTitle>
                 </IonToolbar>
             </IonHeader>
-            <IonContent className='ion-padding'>
-                <div className="ion-padding">
-                    <h2>Alerts!</h2>
+
+            <IonContent class='ion-text-center' >
+                <div>
+                    <h1>ALERTS!</h1>
                 <IonButton onClick={handleSendNotification}>Send me to Application</IonButton>
                 </div>
+
+                <IonList>
+                    {items.map((item, index) => (
+                        <IonItem key={item}>
+                            <IonLabel>{item}</IonLabel>
+                        </IonItem>
+                    ))}
+                </IonList>
+
+                <IonInfiniteScroll>
+                    <IonInfiniteScrollContent loadingText="Please wait..." loadingSpinner="bubbles"></IonInfiniteScrollContent>
+                </IonInfiniteScroll>
             </IonContent>
+
         </IonPage>
     )
 }
