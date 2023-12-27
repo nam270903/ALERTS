7165dd4a528dba2b9dc396624fe7bb32ab166d9f
Le Nhat Minh <minhln.bi12-289@st.usth.edu.vn>
2023-12-27 21:47:39 +0700

    Dashboard commit

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,188 +1,270 @@
 import {
-    IonPage,
-    IonHeader,
-    IonToolbar,
-    IonTitle, 
-    IonContent,
-    IonButtons,
-    IonButton,
-    IonMenuButton
-} from '@ionic/react'
-import { options } from 'ionicons/icons';
-import React, { useState, useEffect } from 'react'
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButtons,
+  IonButton,
+  IonMenuButton,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonCard,
+  IonCardContent,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonBadge,
+  IonIcon,
+  IonSearchbar,
+  IonSelect,
+  IonSelectOption,
+} from '@ionic/react';
+import React, { useState, useEffect } from 'react';
+import './Dashboard.css';
 
-interface Agent{
-    id: string;
-    name: string;
-    platform: string;
-    status: string;
-    node:string;
-    ip: string;
+
+interface Agent {
+  id: string;
+  name: string;
+  platform: string;
+  status: string;
+  node: string;
+  ip: string;
 }
 
 const Dashboard: React.FC = () => {
+  const [searchTerm, setSearchTerm] = useState<string>('');
+  const [statusFilter, setStatusFilter] = useState<string | null>(null);
+  const [jwtToken, setJwtToken] = useState<string>('');
+  const [agents, setAgents] = useState<Agent[]>([]);
+  const [activeAgents, setactiveAgents] = useState<string>('');
+  const [disconectedAgents, setdisconectedAgents] = useState<string>('');
+  const [neverconnectedAgents, setneverconnectedgents] = useState<string>('');
+  const [pendingAgents, setpendingAgents] = useState<string>('');
+  const [totalAgents, settotalAgents] = useState<string>('');
 
-    const [jwtToken,setJwtToken] = useState<string>(''); 
-    const [agents, setAgents] = useState<Agent[]>([]);
-    const [activeAgents, setactiveAgents] = useState<string>('');
-    const [disconectedAgents, setdisconectedAgents] = useState<string>('');
-    const [neverconnectedAgents, setneverconnectedgents] = useState<string>('');
-    const [pendingAgents, setpendingAgents] = useState<string>('');
-    const [totalAgents, settotalAgents] = useState<string>('');
+  const filteredAgents = agents
+  .filter((agent) => agent.name.toLowerCase().includes(searchTerm.toLowerCase()))
+  .filter((agent) => statusFilter === null || agent.status === statusFilter);
 
-    useEffect(() => {
-        const fetchJwtToken = async () => {
-          const options = {
-            method: 'POST',
-            headers: {
-              'User-Agent': 'insomnia/8.4.5',
-              Authorization: 'Basic YXBpLXVzZXI6dkpwLXBzSEtMbVFOWG1TM19qN2U='
-            }
-          };
-    
-          try {
-            const response = await fetch('https://chouette.doclai.com/security/user/authenticate', options);
-            const data = await response.json();
-    
-            if (data && data.data && data.data.token) {
-              setJwtToken(data.data.token);
-            }
-          } catch (error) {
-            console.error(error);
-          }
-        };
-    
-        fetchJwtToken();
-    
-      }, []);
-    
-      useEffect(() => {
-        // Simulating fetching JWT token (replace this with your actual logic)
-        const alerts = async () => {
-          const options = {
-            method: 'GET',
-            headers: {
-              'User-Agent': 'insomnia/8.4.5',
-              Authorization: 'Basic YXBpLXVzZXI6dkpwLXBzSEtMbVFOWG1TM19qN2U='
-            }
-          };
-    
-          try {
-            const response = await fetch('https://chouette.doclai.com/alerts?y=2023&m=12&d=27', options);
-            const data = await response.json();
-            console.log('Fetched Alerts:',data);
-          } catch (error) {
-            console.error(error);
-          }
-        };
-    
-        alerts();
-    
-      }, []);
-    
-      useEffect(() => {
-        if (jwtToken) {
-          const options = {
-            method: 'GET',
-            headers: {
-              Authorization: `Bearer ${jwtToken}`
-            }
-          };
-    
-          const ListAgents = async () => {
-            try {
-              const response = await fetch('https://chouette.doclai.com/agents', options);
-              const data = await response.json();
-              console.log(data);
-              const extractedAgents: Agent[] = data.data.affected_items.map((item: any) => ({
-                id: item.id,
-                name: item.name,
-                ip: item.ip,
-                platform: item.os.platform,
-                status: item.status,
-                node: item.node_name
-              }));
-      
-              setAgents(extractedAgents);
-            } catch (error) {
-              console.error(error);
-            }
-          };
-    
-          const AgentsStatus = async () => {
-            try {
-              const response = await fetch('https://chouette.doclai.com/agents/summary/status', options);
-              const data = await response.json();
-              console.log(data);
-              if (data && data.data) {
-                setactiveAgents(data.data.connection.active);
-                setneverconnectedgents(data.data.connection.never_connected);
-                setdisconectedAgents(data.data.connection.disconnected);
-                setpendingAgents(data.data.connection.pending);
-                settotalAgents(data.data.connection.total);
-              }
-    
-    
-            } catch (error) {
-              console.error(error);
-            }
-          }
-    
-          ListAgents();
-          AgentsStatus();
+  const clearFilter = () => {
+    setStatusFilter(null);
+    setSearchTerm('');
+  };
+
+  useEffect(() => {
+    const fetchJwtToken = async () => {
+      const options = {
+        method: 'POST',
+        headers: {
+          'User-Agent': 'insomnia/8.4.5',
+          Authorization: 'Basic YXBpLXVzZXI6dkpwLXBzSEtMbVFOWG1TM19qN2U='
         }
-      }, [jwtToken]);
+      };
 
-    return (
-        <IonPage>
-          <IonHeader>
-          </IonHeader>
-          <IonContent fullscreen>
-            <IonHeader collapse="condense">
-            </IonHeader>
-            <div>
-          <h1>JWT Token:</h1>
-          <p>{jwtToken}</p>
-            </div>
-    
-            <div>
-          <h1>Agent List</h1>
-          <table>
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>IP Address</th>
-                <th>Platform</th>
-                <th>Cluster Node</th>
-                <th>Status</th>
-    
-              </tr>
-            </thead>
-            <tbody>
-              {agents.map((agent) => (
-                <tr key={agent.id}>
-                  <td>{agent.id}  </td>
-                  <td>{agent.name}  </td>
-                  <td>{agent.ip}  </td>
-                  <td>{agent.platform}  </td>
-                  <td>{agent.node}  </td>
-                  <td>{agent.status}  </td>
-                </tr>
-              ))}
-    
-                <p>Active Agents:</p> {activeAgents}
-                <p>Disconnected Agents:</p> {disconectedAgents}
-                <p>Never Connected Agents:</p> {neverconnectedAgents}
-                <p>Pending Agents:</p> {pendingAgents}
-                <p>Total Agents:</p> {totalAgents}
-    
-            </tbody>
-          </table>
-        </div>
-          </IonContent>
-        </IonPage>
-      )
-}
+      try {
+        const response = await fetch('https://chouette.doclai.com/security/user/authenticate', options);
+        const data = await response.json();
 
-export default Dashboard
+        if (data && data.data && data.data.token) {
+          setJwtToken(data.data.token);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchJwtToken();
+
+  }, []);
+
+  useEffect(() => {
+    const alerts = async () => {
+      const options = {
+        method: 'GET',
+        headers: {
+          'User-Agent': 'insomnia/8.4.5',
+          Authorization: 'Basic YXBpLXVzZXI6dkpwLXBzSEtMbVFOWG1TM19qN2U='
+        }
+      };
+
+      try {
+        const response = await fetch('https://chouette.doclai.com/alerts?y=2023&m=12&d=27', options);
+        const data = await response.json();
+        console.log('Fetched Alerts:',data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    alerts();
+
+  }, []);
+
+  useEffect(() => {
+    if (jwtToken) {
+      const options = {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${jwtToken}`
+        }
+      };
+
+      const ListAgents = async () => {
+        try {
+          const response = await fetch('https://chouette.doclai.com/agents', options);
+          const data = await response.json();
+          console.log(data);
+          const extractedAgents: Agent[] = data.data.affected_items.map((item: any) => ({
+            id: item.id,
+            name: item.name,
+            ip: item.ip,
+            platform: item.os.platform,
+            status: item.status,
+            node: item.node_name
+          }));
+  
+          setAgents(extractedAgents);
+        } catch (error) {
+          console.error(error);
+        }
+      };
+
+      const AgentsStatus = async () => {
+        try {
+          const response = await fetch('https://chouette.doclai.com/agents/summary/status', options);
+          const data = await response.json();
+          console.log(data);
+          if (data && data.data) {
+            setactiveAgents(data.data.connection.active);
+            setneverconnectedgents(data.data.connection.never_connected);
+            setdisconectedAgents(data.data.connection.disconnected);
+            setpendingAgents(data.data.connection.pending);
+            settotalAgents(data.data.connection.total);
+          }
+
+
+        } catch (error) {
+          console.error(error);
+        }
+      }
+
+      ListAgents();
+      AgentsStatus();
+    }
+  }, [jwtToken]);
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonMenuButton />
+          </IonButtons>
+          <IonTitle>Dashboard</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Dashboard</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+
+        <IonGrid className="ion-text-center">
+          <IonRow>
+            <IonCol>
+              <h1>Agent List</h1>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+
+        <IonCard>
+          <IonCardContent>
+            <IonGrid>
+              <IonRow>
+                <IonCol>
+                  <IonLabel>Active:</IonLabel>
+                  <IonBadge color="success">{activeAgents}</IonBadge>
+                </IonCol>
+                <IonCol>
+                  <IonLabel>Disconnected:</IonLabel>
+                  <IonBadge color="danger">{disconectedAgents}</IonBadge>
+                </IonCol>
+              </IonRow>
+              <IonRow>
+                <IonCol>
+                  <IonLabel>Never Connected</IonLabel>
+                  <IonBadge color="warning">{neverconnectedAgents}</IonBadge>
+                </IonCol>
+                <IonCol>
+                  <IonLabel>Pending:</IonLabel>
+                  <IonBadge color="primary">{pendingAgents}</IonBadge>
+                </IonCol>
+              </IonRow>
+              <IonRow>
+                <IonCol>
+                  <IonLabel>Total Agents:</IonLabel>
+                  <IonBadge color="dark">{totalAgents}</IonBadge>
+                </IonCol>
+              </IonRow>
+            </IonGrid>
+          </IonCardContent>
+        </IonCard>
+
+        <IonGrid className="search-and-filter">
+          <IonRow>
+            <IonCol size="8">
+              <IonSearchbar
+                value={searchTerm}
+                onIonChange={(e) => setSearchTerm(e.detail.value!)}
+              ></IonSearchbar>
+            </IonCol>
+            <IonCol size="4" className="ion-text-end">
+              <IonSelect
+                value={statusFilter}
+                placeholder="Filter by Status"
+                onIonChange={(e) => setStatusFilter(e.detail.value)}
+                className="status-filter"
+              >
+                <IonSelectOption value="active">Active</IonSelectOption>
+                <IonSelectOption value="disconnected">Disconnected</IonSelectOption>
+                <IonSelectOption value="pending">Pending</IonSelectOption>
+                <IonSelectOption value="never_connected">Never Connected</IonSelectOption>
+              </IonSelect>
+
+              <IonButton onClick={clearFilter}>
+                  Clear Filter
+                </IonButton>
+
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+
+        <IonList>
+          {filteredAgents.map((agent) => (
+            <IonItem key={agent.id}>
+              <IonLabel>
+                <h2>{agent.name}</h2>
+                <p>ID: {agent.id}</p>
+                <p>IP Address: {agent.ip}</p>
+                <p>Platform: {agent.platform}</p>
+                <p>Cluster Node: {agent.node}</p>
+                <p>Status: {agent.status}</p>
+              </IonLabel>
+
+              <IonBadge color={agent.status === 'active' ? 'success' : agent.status === 'disconnected' ? 'danger' : 'warning'}>
+                {agent.status === 'active' ? 'Active' : agent.status === 'disconnected' ? 'Disconnect' : 'N'}
+              </IonBadge>
+
+            </IonItem>
+          ))}
+        </IonList>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Dashboard;
