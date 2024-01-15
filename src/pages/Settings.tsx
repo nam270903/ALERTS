82b1327db57b0413aecb70bc75d2e29945c711d4
nam270903 <nam270903@gmail.com>
2024-01-16 02:33:29 +0700

    add function sign out

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -67,31 +67,24 @@ const Settings: React.FC = () => {
 
   const [themeToggle, setThemeToggle] = useState(false);
 
-  // Listen for the toggle check/uncheck to toggle the dark theme
   const toggleChange = (ev: ToggleCustomEvent) => {
     toggleDarkTheme(ev.detail.checked);
   };
 
-  // Add or remove the "dark" class on the document body
   const toggleDarkTheme = (shouldAdd: boolean) => {
     document.body.classList.toggle('dark', shouldAdd);
   };
 
-  // Check/uncheck the toggle and update the theme based on isDark
   const initializeDarkTheme = (isDark: boolean) => {
     setThemeToggle(isDark);
     toggleDarkTheme(isDark);
   };
 
   useEffect(() => {
-    // Use matchMedia to check the user preference
     const prefersDark = window.matchMedia('(prefers-color-scheme: light)');
 
-    // Initialize the dark theme based on the initial
-    // value of the prefers-color-scheme media query
     initializeDarkTheme(prefersDark.matches);
 
-    // Listen for changes to the prefers-color-scheme media query
     prefersDark.addEventListener('change', (mediaQuery) => initializeDarkTheme(mediaQuery.matches));
   }, []);
 
@@ -160,26 +153,26 @@ const Settings: React.FC = () => {
         <IonCard>
           <IonCardContent>
             <IonGrid>
-          <IonList inset={true}>
-            <IonItem>
-              <IonToggle checked={themeToggle} onIonChange={toggleChange} justify="space-between">Dark Mode</IonToggle>
-            </IonItem>
-          </IonList>
-          </IonGrid>
+              <IonList inset={true}>
+                <IonItem>
+                  <IonToggle checked={themeToggle} onIonChange={toggleChange} justify="space-between">Dark Mode</IonToggle>
+                </IonItem>
+              </IonList>
+            </IonGrid>
           </IonCardContent>
         </IonCard>
       </IonContent>
 
 
-      <IonButton className='ion-button'>
-        Sign out
-      </IonButton>
+      <IonButton onClick={handleLogout} expand="full">
+        <IonIcon icon={logOutOutline} slot="start" />
+          Sign out
+        </IonButton>
 
       <LogoutAlert
         isOpen={showLogoutAlert}
         onDidDismiss={() => setShowLogoutAlert(false)}
-        onLogoutConfirmed={confirmLogout}
-      />
+        onLogoutConfirmed={confirmLogout}/>
     </IonPage>
   );
 };
