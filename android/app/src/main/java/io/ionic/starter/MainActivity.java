d3024565450ace68f35439ff318aaaaecc4352b4
TensingNightco <thanh.hocong@gmail.com>
2024-01-29 16:18:49 +0700

    Changed logo and add some configurations to enable push notifications

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,14 +1,5 @@
 package io.ionic.starter;
 
-import static androidx.core.app.ActivityCompat.shouldShowRequestPermissionRationale;
-
-import android.content.pm.PackageManager;
-import android.os.Build;
-
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.core.content.ContextCompat;
-
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
